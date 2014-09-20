528213e08d16912f4664712a04b21657e85ecd86
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-09-21 02:44:52 +0900

    mobile postDetail done

--- static_html/mobile/post_detail.php
+++ static_html/mobile/post_detail.php
@@ -17,6 +17,20 @@
         <form id="commentForm" action="index.html" method="post">
           <input type="text" name="comment" placeholder="コメントを入力" /><input type="submit" name="sendComment" value="送信" />
         </form>
+
+        <? for ($i = 0; $i < 8; $i++): ?>
+        <article class="comments">
+          <div class="commentUserIcon">
+            <img src="img/masaki.jpg" alt="userIcon" />
+          </div>
+          <div class="comment">
+            <h4><a href="#">正木 貴大</a></h4>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+        </article>
+        <? endfor; ?>
       </section>
   </section>
 
