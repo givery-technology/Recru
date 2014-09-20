02b051646341f197d3e19a84d10b737e164eda8e
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-09-20 17:37:18 +0900

    mobile index

---
+++ static_html/mobile/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/mobile.css">
+  <title>サービスタイトル</title>
+</head>
+<body>
+
+<header>
+  <div id="headerWrap">
+    <h1><a href="#">logo</a></h1>
+    <div id="setting"><a href="#"><img src="img/setting.png" alt="setting" /></a></div>
+  </div>
+</header>
