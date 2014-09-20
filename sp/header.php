060af3ca5c4dd81e6c02bcb34bfc5c91e595d2a9
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-09-21 03:57:05 +0900

    【※注意※】ディレクトリ構造の変更 cssによるサイドメニューの表示

---
+++ sp/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/mobile.css">
+  <title>サービスタイトル</title>
+</head>
+<body>
+
+<header id="header">
+  <div id="sideMenuBtn"><label for="side"><img src="img/side.png" alt="side" /></label></div>
+  <h1><a href="#">logo</a></h1>
+  <div id="setting"><a href="#"><img src="img/setting-white.png" alt="setting" /></a></div>
+</header>
+
+<input type="checkbox" id="side">
+<aside id="sideMenu">
+  <ul>
+    <li><a href="#">マイページ</a></li>
+    <li><a href="#">友達一覧</a></li>
+    <li><a href="#">企業一覧</a></li>
+  </ul>
+</aside>
--- sp/index.php
+++ sp/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../css/mobile.css">
+  <link rel="stylesheet" href="css/mobile.css">
   <title>サービスタイトル</title>
 </head>
 <body>
