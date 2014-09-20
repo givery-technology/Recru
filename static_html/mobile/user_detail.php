3e1624cef27b26cf54c3af5b1710548537e5f985
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-09-20 22:45:35 +0900

    user_detail

---
+++ static_html/mobile/user_detail.php
@@ -0,0 +1,31 @@
+<? require_once "header.php"; ?>
+
+  <section id="wrapper">
+    <article id="userDetail">
+      <header>
+        <h2>正木　貴大</h2>
+        <div id="userIcon"><img src="img/masaki.jpg" alt="" /></div>
+        <div id="userInfo">
+          <div id="school">法政大学 16卒</div>
+          <div id="postCount">投稿数 : 9</div>
+        </div>
+      </header>
+    </article>
+    <section id="userPosts">
+      <h2>最近の投稿</h2>
+
+      <article class="posts">
+        <div class="companyName"><a href="#">株式会社ギブリー</a></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <section class="commentArea">
+          <ul>
+            <li><a href="#">コメントする</a></li>
+            <li><a href="#">詳細</a></li>
+          </ul>
+        </section>
+      </article>
+
+    </section>
+  </section>
+
+<? require_once "footer.php"; ?>
