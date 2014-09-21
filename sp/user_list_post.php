512c38f0254f1914149896c3df21012265d1e297
ami1029 <ami.kea@me.com>
2014-09-21 23:10:56 +0900

    ともだちつくる

---
+++ sp/user_list_post.php
@@ -0,0 +1,43 @@
+<?php
+ini_set("display_errors", On);
+error_reporting(E_ALL);
+
+mb_language("uni");
+mb_internal_encoding("utf-8");
+mb_http_input("auto");
+mb_http_output("utf-8");
+
+require "constant.php";
+
+//?id=ほげほげ　で指定する！
+if(isset($_GET['id'])){
+  $usr_id = $_GET['id'];
+}
+
+$sql = "SELECT user.name, user.img, user.univ FROM user_friend INNER JOIN user ON user_friend.user_id = user.id WHERE user_friend.user_id=".$usr_id;
+$link = new mysqli("localhost", "$db_usr", "$db_pwd", "$db_name");
+
+$link->set_charset('utf8');
+
+if(mysqli_connect_errno()) {
+  printf("connect failed: %s\n", $link->connect_error());
+  exit();
+}
+
+$user= array();
+if($result = mysqli_query($link, $sql)){
+  while ($row = mysqli_fetch_object($result)){
+    $user[] = array(
+      'name' => $row->name
+      ,'img'=> $row->img
+      ,'univ'=> $row->univ
+    );
+  }
+  mysqli_free_result($result);
+}
+// header('Content-type: application/json');
+echo json_encode($user);
+
+mysqli_close($link);
+
+?>
