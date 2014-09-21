7bd81bcdf9c7c9085f8270d188727ffd43dd8204
ami1029 <ami.kea@me.com>
2014-09-21 23:00:32 +0900

    修正！！　ฅ( ˘ω˘ )ฅ　名前でみれます

--- sp/timeline_post.php
+++ sp/timeline_post.php
@@ -10,7 +10,7 @@ mb_http_output("utf-8");
 require "constant.php";
 
 
-$sql = "SELECT id, user_id, company_id, category_id, comment, created_at FROM contribution ORDER BY created_at";
+$sql = "SELECT contribution.id, user.name, user.img, company.name as company_name, category.name as category_name, contribution.comment, contribution.created_at FROM contribution INNER JOIN user ON contribution.user_id = user.id INNER JOIN company ON contribution.company_id = company.id INNER JOIN category ON contribution.category_id = category.id ORDER BY contribution.created_at";
 //$sql = "SELECT id, user_id, company_id, category_id, comment, created_at FROM contribution ORDER BY created_at DESC LIMIT 20";
 
 $link = new mysqli("localhost", "$db_usr", "$db_pwd", "$db_name");
@@ -25,9 +25,9 @@ if($result = mysqli_query($link, $sql)){
   while ($row = mysqli_fetch_object($result)){
     $user[] = array(
     'id' => $row->id
-    ,'user_id'=> $row->user_id
-    ,'company_id'=> $row->company_id
-    ,'category_id' => $row->category_id
+    ,'img'=> $row->img
+    ,'company_name'=> $row->company_name
+    ,'category_name' => $row->category_name
     ,'comment' => $row->comment
     ,'created_at' => $row->created_at
     );
